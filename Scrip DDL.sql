28b239a87e910b4907f91b1b6cb1966a3f144d87
Xiomaraht <xiomy6789@gmail.com>
2025-05-17 20:54:23 -0500

    commit final

--- Scrip DDL.sql
+++ Scrip DDL.sql
@@ -4,16 +4,16 @@ create database Prueba_2;
 use Prueba_2;
 
 create table Fabricante(
-	idfabricante int primary key auto_increment,
+	codigo_fabricante int primary key auto_increment,
     nombre varchar(100)
 );
 
 create table Producto(
-	idproducto int primary key auto_increment,
+	codigo int primary key auto_increment,
     nombre varchar (100),
     precio double,
-    idfabricante int
+    codigo_fabricante int
 );
 
 alter table Producto
-add foreign key  (idfabricante) references Fabricante(idfabricante);
+add foreign key  (codigo_fabricante) references Fabricante(codigo_fabricante);
